a8fb612c53d0a0ab258794d583b819bc1f87f6fc
Joan Rojas <Joan.rojas@vasslatam.com>
2022-10-18 21:42:34 -0500

    Creacion formulario

--- src/app/componentes/booking/booking.module.ts
+++ src/app/componentes/booking/booking.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookingComponent } from './booking.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -9,7 +10,8 @@ import { BookingComponent } from './booking.component';
     BookingComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   exports: [
     BookingComponent
